4193660a21d71de2d1d67ad0b572081adfb2b316
Magic <fiosiomagic@gmail.com>
2025-04-15 09:12:50 +0300

    Додаємо тестову зміну

--- poems/utils/customizer.py
+++ poems/utils/customizer.py
@@ -1,6 +1,7 @@
 def apply_theme(user, theme_name):
     """
     Застосовує тему до користувача і зберігає її.
+    Тестова зміна.
     """
     if user.is_authenticated:
         user.theme = theme_name
